bea5c37313dc07caa12cab49a2afeb668269c325
Kristen Bachman <kristen.bachman@udri.udayton.edu>
2018-05-23 19:18:41 -0400

    Initial commit

---
+++ words.js
@@ -0,0 +1,18 @@
+//replace 'change me', but keep the quotation marks!
+var adjective1 = "change me"; 
+var adjective2 = "change me"; 
+var noun3 = "change me"; 
+var verbEndingInIng4 = "change me"; 
+var pluralNoun5 = "change me"; 
+var pluralPartOfTheBody6 = "change me"; 
+var verbEndingInIng7 = "change me"; 
+var noun8 = "change me"; 
+var typeOfInexpensiveFood9 = "change me"; 
+var pluralNoun10 = "change me"; 
+var pluralNoun11 = "change me"; 
+var number12 = "change me"; 
+var noun13 = "change me"; 
+var partOfTheBody14 = "change me"; 
+var noun15 = "change me"; 
+var adverb16 = "change me"; 
+var noun17 = "change me"; 
